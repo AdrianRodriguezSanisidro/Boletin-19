ec87da4bc4d667b066871682ab552292e5725dcc
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-02-07 09:24:52 +0100

    Crear constructores,getters,setters y toString

--- Boletin 19/src/boletin/pkg19/Libro.java
+++ Boletin 19/src/boletin/pkg19/Libro.java
@@ -5,5 +5,64 @@ package boletin.pkg19;
  * @author Adry
  */
 public class Libro {
+    String titulo,autor,ISBN;
+    float prezo;
+    int numUnidades;
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String autor, String ISBN, float prezo, int numUnidades) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.prezo = prezo;
+        this.numUnidades = numUnidades;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public float getPrezo() {
+        return prezo;
+    }
+
+    public void setPrezo(float prezo) {
+        this.prezo = prezo;
+    }
+
+    public int getNumUnidades() {
+        return numUnidades;
+    }
+
+    public void setNumUnidades(int numUnidades) {
+        this.numUnidades = numUnidades;
+    }
+
+    @Override
+    public String toString() {
+        return "Titulo=" + titulo + ", Autor=" + autor + ", ISBN=" + ISBN + ", prezo=" + prezo + ", numUnidades=" + numUnidades;
+    }
     
 }
