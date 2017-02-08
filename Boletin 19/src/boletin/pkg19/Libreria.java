a0bc66056fd48bc47e60aa6d1006246194a1fec0
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-02-08 08:50:09 +0100

    Metodo para pedir los datos

--- Boletin 19/src/boletin/pkg19/Libreria.java
+++ Boletin 19/src/boletin/pkg19/Libreria.java
@@ -1,9 +1,16 @@
 package boletin.pkg19;
 
+import java.util.ArrayList;
+import static Sanisidro.PedirDatos.pedirFloat;
+import static Sanisidro.PedirDatos.pedirInt;
+import static Sanisidro.PedirDatos.pedirString;
 /**
  *
  * @author Adry
  */
 public class Libreria {
-    
+    ArrayList <Libro> libreria=new ArrayList();
+    public void engadirLibros (){
+        libreria.add(new Libro(pedirString(),pedirString(),pedirString(),pedirFloat(),pedirInt()));
+    }
 }
