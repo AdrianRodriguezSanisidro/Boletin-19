fc549e0d2479dace46754015a0cb2f80ec09d291
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-02-09 10:50:29 +0100

    Metodo vender bien hecho

--- Boletin 19/src/boletin/pkg19/Libreria.java
+++ Boletin 19/src/boletin/pkg19/Libreria.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import static Sanisidro.PedirDatos.pedirFloat;
 import static Sanisidro.PedirDatos.pedirInt;
 import static Sanisidro.PedirDatos.pedirString;
+import java.util.Collections;
+import java.util.Iterator;
 /**
  *
  * @author Adry
@@ -13,8 +15,24 @@ public class Libreria {
     public void engadirLibros (){
         libreria.add(new Libro(pedirString(),pedirString(),pedirString(),pedirFloat(),pedirInt()));
     }
-    public void borrarLibro(Libro l,ArrayList<Libro>libreria){
-        int index=libreria.indexOf(l);
-        libreria.remove(index);
+    public void venderLibro(){
+        Libro ventaLibro=new Libro(pedirString(),pedirString(),pedirString(),pedirFloat(),pedirInt());
+        for(int i=0;i<libreria.size();i++){
+            if(ventaLibro.ISBN.equals(libreria.get(i).getISBN())){
+                if (libreria.get(i).getNumUnidades()>=ventaLibro.getNumUnidades()){
+                    libreria.get(i).setNumUnidades(libreria.get(i).numUnidades-ventaLibro.numUnidades);
+                }else
+                    System.out.println("No quedan suficientes unidades");
+            }
+        }
+    }
+    public void amosarLibros(){
+       Iterator<Libro>libroIterator=libreria.iterator();
+       while(libroIterator.hasNext()){
+           System.out.println(libroIterator.next());
+       }
+    }
+    public void darDeBaixa(){
+        
     }
 }
