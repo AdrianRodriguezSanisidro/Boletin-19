9b36e9ecb72f464bbece95d462e3f12ec305d1ee
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-02-09 11:22:59 +0100

    Switch case terminado,final del boletin

--- Boletin 19/src/boletin/pkg19/Boletin19.java
+++ Boletin 19/src/boletin/pkg19/Boletin19.java
@@ -1,5 +1,7 @@
 package boletin.pkg19;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Adry
@@ -7,7 +9,32 @@ package boletin.pkg19;
 public class Boletin19 {
 
     public static void main(String[] args) {
-       
+       /*Libreria libro1=new Libreria();
+       Libreria libro2=new Libreria();
+       Libreria libro3=new Libreria();
+       libro1.engadirLibros();
+       libro2.engadirLibros();
+       libro3.engadirLibros();*/
+       Libreria objLibro=new Libreria();
+       int opcion;
+       do {
+           opcion=Integer.parseInt(JOptionPane.showInputDialog("Selecciona la opcion que quieras realizar:\n1=Añadir libro\n2=Borrar libro\n3=Amosar todos os libros\n4=Dar de baixa\n5=Consultar libro"));
+           
+           switch(opcion){
+           case 1:objLibro.engadirLibros();
+           break;
+           case 2:objLibro.venderLibro();
+           break;
+           case 3:objLibro.amosarLibros();
+           break;
+           case 4:objLibro.darDeBaixa();
+           break;
+           case 5:objLibro.consultarLibro();
+           break;
+           case 6:System.exit(0);
+       }
+               
+       }while(opcion<6||opcion>0);
     }
     
 }
